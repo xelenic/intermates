10aac7d31c306563c8bd43f09849b61076d868a0
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2023-09-24 00:13:09 +0530

    Dashboard Update Welcome Screen

---
+++ resources/views/dashboard/welcome_screen/index.blade.php
@@ -0,0 +1,38 @@
+<div class="mb-3">
+    <div class="row mb-2 g-3 g-mb-4">
+        <div class="col">
+            <div class="p-4 bg-white rounded shadow-sm h-100 d-flex flex-column">
+                <small class="text-muted d-block mb-1">My Wallet</small>
+                <p class="h3 text-black fw-light mt-auto">
+                   0
+                </p>
+            </div>
+        </div>
+        <div class="col">
+            <div class="p-4 bg-white rounded shadow-sm h-100 d-flex flex-column">
+                <small class="text-muted d-block mb-1">Buy</small>
+                <p class="h3 text-black fw-light mt-auto">
+                    24,668
+                </p>
+            </div>
+        </div>
+        <div class="col">
+            <div class="p-4 bg-white rounded shadow-sm h-100 d-flex flex-column">
+                <small class="text-muted d-block mb-1">Sell</small>
+                <p class="h3 text-black fw-light mt-auto">
+                    10,000
+
+                </p>
+            </div>
+        </div>
+        <div class="col">
+            <div class="p-4 bg-white rounded shadow-sm h-100 d-flex flex-column">
+                <small class="text-muted d-block mb-1">My Coins Safe Room</small>
+                <p class="h3 text-black fw-light mt-auto">
+                    65,661
+
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
