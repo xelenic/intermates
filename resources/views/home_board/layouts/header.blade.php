4f556c8d82fc511a4f29453a39764fa861719fb1
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2023-09-24 15:55:52 +0530

    HEader Updates

---
+++ resources/views/home_board/layouts/header.blade.php
@@ -0,0 +1,23 @@
+<!-- App Header -->
+<div class="appHeader bg-primary text-light">
+    <div class="left">
+        <a href="#" class="headerButton" data-bs-toggle="modal" data-bs-target="#sidebarPanel">
+            <ion-icon name="menu-outline"></ion-icon>
+        </a>
+    </div>
+    <div class="pageTitle">
+        <img src="{{url('main_portal/img/logo.png')}}" alt="logo" class="logo">
+    </div>
+    <div class="right">
+        <a href="app-notifications.html" class="headerButton">
+            <ion-icon class="icon" name="notifications-outline"></ion-icon>
+            <span class="badge badge-danger">4</span>
+        </a>
+        <a href="app-settings.html" class="headerButton">
+            <img src="{{url('main_portal/img/sample/avatar/avatar1.jpg')}}" alt="image" class="imaged w32">
+            <span class="badge badge-danger">6</span>
+        </a>
+    </div>
+</div>
+<!-- * App Header -->
+
